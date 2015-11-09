b93fd4c001a275444cfb6b9d0613635d880fb5c0
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-11-09 22:19:39 +0100

    Cleanup

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+var socket = io();
+
+socket.on('files', function (msg) {
+    $('#messages').append($('<li>').text(msg.files));
+//    console.log(msg);
+});
+socket.on('change', function (msg) {
+    $('#messages').append($('<li>').text(msg));
+});
