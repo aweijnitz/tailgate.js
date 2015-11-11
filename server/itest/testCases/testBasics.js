29e0910b46f5bb049f77a39456471de75fd31161
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-11-11 22:39:37 +0100

    Fixed integration tests

--- server/itest/testCases/testBasics.js
+++ server/itest/testCases/testBasics.js
@@ -1,18 +1,20 @@
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-    casper.start("http://www.google.fr/", function() {
-        test.assertTitle("Google", "google homepage title is the one expected");
-        test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "casperjs"
+casper.test.begin('Client can tail a file', 2, function suite(test) {
+    casper.start("http://localhost:3001/test0.html", function() {
+        test.assertTitle("Test-0", "Page title is the one expected");
+        test.assertExists('form[id="fileName"]', "fileName form is found");
+        this.fillSelectors('form[id="fileName"]', {
+            'input#tailFile': 'casperjs'
         }, true);
     });
 
     casper.then(function() {
+        /*
         test.assertTitle("casperjs - Recherche Google", "google title is ok");
         test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
         test.assertEval(function() {
             return __utils__.findAll("h3.r").length >= 10;
         }, "google search for \"casperjs\" retrieves 10 or more results");
+        */
     });
 
     casper.run(function() {
